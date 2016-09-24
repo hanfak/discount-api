a9d6d3e57434423b65e20530badb477dd38b396e
Hanif Fakira <fakira.work@gmail.com>
2016-09-24 11:52:54 +0100

    setup feature tests

---
+++ example/spec/features/feature_spec.rb
@@ -0,0 +1,40 @@
+require "spec_helper"
+
+feature 'Example 1' do
+  let(:standard_delivery) { Delivery.new(:standard, 10) }
+  let(:express_delivery) { Delivery.new(:express, 20) }
+  let(:broadcaster_1) {Broadcaster.new(1, 'Viacom')}
+  let(:broadcaster_2) {Broadcaster.new(2, 'Disney')}
+  let(:broadcaster_3) {Broadcaster.new(3, 'Discovery')}
+  let(:broadcaster_4) {Broadcaster.new(4, 'Horse and Country')}
+  let(:material) { Material.new 'WNP/SWCL001/010' }
+  let(:order) { Order.new material }
+
+  scenario 'applies correct discount and total is 45.00' do
+    order.add broadcaster_1, standard_delivery
+    order.add broadcaster_2, standard_delivery
+    order.add broadcaster_3, standard_delivery
+    order.add broadcaster_4, express_delivery
+
+    expect(order.total_cost).to eq(45.00)
+  end
+end
+
+# feature 'Example 2' do
+#   let(:standard_delivery) { Delivery.new(:standard, 10) }
+#   let(:express_delivery) { Delivery.new(:express, 20) }
+#   let(:broadcaster_1) {Broadcaster.new(1, 'Viacom')}
+#   let(:broadcaster_2) {Broadcaster.new(2, 'Disney')}
+#   let(:broadcaster_3) {Broadcaster.new(3, 'Discovery')}
+#   let(:broadcaster_4) {Broadcaster.new(4, 'Horse and Country')}
+#   let(:material) { Material.new 'ZDW/EOWW005/010' }
+#   let(:order) { Order.new material }
+#
+#   scenario 'applies correct discount and total is 40.50' do
+#     order.add broadcaster_1, express_delivery
+#     order.add broadcaster_2, express_delivery
+#     order.add broadcaster_3, express_delivery
+#
+#     expect(order.total_cost).to eq(40.50)
+#   end
+end
--- example/spec/spec_helper.rb
+++ example/spec/spec_helper.rb
@@ -16,6 +16,11 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'capybara/rspec'
+require './models/broadcaster'
+require './models/delivery'
+require './models/material'
+require './models/order'
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
